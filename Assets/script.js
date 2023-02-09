ce3c33c853624294b3b4cd71aac67955948348bf
Matthew Baty <mattbaty@outlook.com>
2023-02-08 20:18:08 -0500

    added vars, my brain hurts

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatePassword() {
+  for (i - 0; i < passwordLengthNum++; i++) {
+  }
 }
-
-const characterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const characterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const characterSpecial = ["!", "@", "#", "$", "%", "^", "*"];
-const characterNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // click button to begin generating password 
 if ( confirm("Would you like to generate a password?") == true) {
@@ -25,7 +13,33 @@ if ( confirm("Would you like to generate a password?") == true) {
   passwordLengthNum = parseInt(passwordLength);
 }
 
+var lowercase = window.confirm("Would you like your password to include lowercase characters?");
+var uppercase = window.confirm("Would you like your password to include uppercase characters?");
+var numeric = window.confirm("Would you like your password to include numbers?");
+var specialChar = window.confirm("Would you like your password to inlcude special characters?");
+
+const lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialList = ["!", "@", "#", "$", "%", "^", "*"];
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
+}
+
 console.log(passwordLengthNum);
 // for loop to index array for passwordLengthNum
 
-
+// prompts for pw requirements: LENGTH (8-128); LOWERCASE; UPPERCASE; NUMERIC; SPECIAL
+// return [Math.floor(Math.random(charactersUpper.length))]
+// return [Math.floor(Math.random(charactersLower.length))]
+// return [Math.floor(Math.random(charactersSpecial.length))]
+// return [Math.floor(Math.random(characterNumber.length))]
