a1ee186baccc53ab79bf1daf773528f2ae35c29c
Matthew Baty <mattbaty@outlook.com>
2023-02-08 19:40:00 -0500

    first round of character edits

--- Assets/script.js
+++ Assets/script.js
@@ -10,5 +10,22 @@ function writePassword() {
 
 }
 
+const characterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const characterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const characterSpecial = ["!", "@", "#", "$", "%", "^", "*"];
+const characterNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// click button to begin generating password 
+if ( confirm("Would you like to generate a password?") == true) {
+  passwordLength = prompt("Between 8 and 128 characters, how many characters would like the password to be?", "8-128");
+
+  passwordLengthNum = parseInt(passwordLength);
+}
+
+console.log(passwordLengthNum);
+// for loop to index array for passwordLengthNum
+
+
