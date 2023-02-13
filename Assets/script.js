f4f8c8abb9bd433d612079d0fa3ef05c7970dd2d
Matthew Baty <mattbaty@outlook.com>
2023-02-13 18:57:02 -0500

    final comments and clean up

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// whole function to generate password
 function generatePassword() {
   var passLength = window.prompt("How long do you want your password to be?");
 
@@ -8,8 +9,7 @@ function generatePassword() {
     window.alert("Your password must be between 8 and 128 characters.")
     return;
   }
-
-  // user questions for possible password characters
+  // declared variables
   var lowercase, uppercase, numeric, specialChar;
   // character sets
   const lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -18,11 +18,12 @@ function generatePassword() {
   const specialList = ["!", "@", "#", "$", "%", "^", "*"];
 
   var passwordContainer = [];
+  //built function that forces the user to select at least one character type, will continue to loop until one is chosen. 
   function getPasswordDetails(){
     lowercase = window.confirm("Would you like your password to include lowercase characters?");
     uppercase = window.confirm("Would you like your password to include uppercase characters?");
     numeric = window.confirm("Would you like your password to include numbers?");
-    specialChar = window.confirm("Would you like your password to inlcude special characters?");
+    specialChar = window.confirm("Would you like your password to include special characters?");
     if (!lowercase && !uppercase && !numeric && !specialChar) {
       window.alert("You must select at least one character type!");
       getPasswordDetails();
@@ -46,10 +47,6 @@ function generatePassword() {
 
   }
   getPasswordDetails();
-  // // this will use lowercase characters if the user did not confirm any prompts 
-  // if (passLength.length === 0) {
-  //   passwordContainer.push(lowercaseList);
-  // }
 
   var generatedPassword = "";
   // for loop to create the password
